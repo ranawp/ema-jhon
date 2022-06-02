553520096f8935e869d4d9d896698f0dcb3a2459
ranawp <ranasaha4224#gmail.com>
2022-06-02 11:13:08 +0600

    navbar added

---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../Header/Header.css'
+import logo from '../../../assets/images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='navbar_bg'>
+            <div className='navbar'>
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div>
+                    <nav>
+                        <a href="/order">Order</a>
+                        <a href="/review">Order Review</a>
+                        <a href="/inventories">Manage Inventory</a>
+                        <a href="/login">Login</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
